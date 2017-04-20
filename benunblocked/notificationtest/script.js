cb107750afb721ad515e905d5cc93f506292062c
ptoil <suhailmallick@gmail.com>
2017-04-20 09:05:42 -0400

    .then with result

--- benunblocked/notificationtest/script.js
+++ benunblocked/notificationtest/script.js
@@ -5,21 +5,25 @@ if (!("Notification" in window)) {
 // Let's check whether notification permissions have already been granted
 else if (Notification.permission === "granted") {
 	// If it's okay let's create a notification
-	document.getElementById("text").innerHTML += "     permission already granted;";
+	document.getElementById("text").innerHTML += " permission already granted;";
 	var notification = new Notification("Hi there!");
 }
 
 // Otherwise, we need to ask the user for permission
 else if (Notification.permission !== 'denied') {
-	document.getElementById("text").innerHTML += "     permission denied;";
+	document.getElementById("text").innerHTML += " permission denied;";
 	Notification.requestPermission(function (permission) {
 		// If the user accepts, let's create a notification
-		document.getElementById("text").innerHTML += "     permission requested;";
+		document.getElementById("text").innerHTML += " permission requested;";
 		if (permission === "granted") {
-			document.getElementById("text").innerHTML += "     permission granted;";
+			document.getElementById("text").innerHTML += " permission granted;";
 			setTimeout(function(){
-				var notification = new Notification("Hi there!");
-				document.getElementById("text").innerHTML += "     notfication created;";
+				//var notification = new Notification("Hi there!");
+				
+				Notification.requestPermission().then(function(result) {
+					document.getElementById("text").innerHTML += " notfication created; " + result + ";";
+					
+				});
 			}, 5000);
 		}
 	});
