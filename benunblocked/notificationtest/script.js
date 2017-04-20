af2f1dec03c1d7578a80c6d3ffce19fd5dd63210
ptoil <suhailmallick@gmail.com>
2017-04-20 09:00:00 -0400

    add getElementByIds for good measure

--- benunblocked/notificationtest/script.js
+++ benunblocked/notificationtest/script.js
@@ -5,21 +5,21 @@ if (!("Notification" in window)) {
 // Let's check whether notification permissions have already been granted
 else if (Notification.permission === "granted") {
 	// If it's okay let's create a notification
-	text.innerHTML += "     permission already granted;";
+	document.getElementById("text").innerHTML += "     permission already granted;";
 	var notification = new Notification("Hi there!");
 }
 
 // Otherwise, we need to ask the user for permission
 else if (Notification.permission !== 'denied') {
-	text.innerHTML += "     permission denied;";
+	document.getElementById("text").innerHTML += "     permission denied;";
 	Notification.requestPermission(function (permission) {
 		// If the user accepts, let's create a notification
-		text.innerHTML += "     permission requested;";
+		document.getElementById("text").innerHTML += "     permission requested;";
 		if (permission === "granted") {
-			text.innerHTML += "     permission granted;";
+			document.getElementById("text").innerHTML += "     permission granted;";
 			setTimeout(function(){
 				var notification = new Notification("Hi there!");
-				text.innerHTML += "     notfication created;";
+				document.getElementById("text").innerHTML += "     notfication created;";
 			}, 5000);
 		}
 	});
